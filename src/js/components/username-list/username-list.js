b9dcaf2f4d8efbe6f68f633360214763659da5dc
rp222nc <rp222nc@student.lnu.se>
2024-09-09 21:33:40 +0200

    move files

--- src/js/components/username-list/username-list.js
+++ src/js/components/username-list/username-list.js
@@ -188,15 +188,15 @@ customElements.define('username-list',
 
           const copyButton = document.createElement('input')
           copyButton.setAttribute('type', 'image')
-          copyButton.setAttribute('src', '/img/copy_symbol.svg')
+          copyButton.setAttribute('src', '/copy_symbol.svg')
           copyButton.setAttribute('class', 'copy-button')
           const usernameText = document.createElement('span')
           usernameText.textContent = username
 
           copyButton.addEventListener('click', () => {
             navigator.clipboard.writeText(username).then(() => {
-              copyButton.setAttribute('src', '/img/hyperlink-icon.svg')
-              setTimeout(() => copyButton.setAttribute('src', '/img/copy_symbol.svg'), 500)
+              copyButton.setAttribute('src', '/hyperlink-icon.svg')
+              setTimeout(() => copyButton.setAttribute('src', '/copy_symbol.svg'), 500)
             }).catch(err => {
               console.error('Failed to copy: ' + err)
             })
